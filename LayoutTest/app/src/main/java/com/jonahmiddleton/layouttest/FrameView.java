e0a88cead90f1dd28308c4277b78a899d5fbbdcc
Jonah Middleton <jonah.middleton@gmail.com>
2021-07-24 14:43:08 -0400

    Create layout test app

---
+++ LayoutTest/app/src/main/java/com/jonahmiddleton/layouttest/FrameView.java
@@ -0,0 +1,14 @@
+package com.jonahmiddleton.layouttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FrameView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_view);
+    }
+}
